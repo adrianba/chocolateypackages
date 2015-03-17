5078291c6965ca1954c4739e375c2471270f460f
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-17 14:41:16 -0400

    installers and uninstallers

--- packages/wudt/tools/chocolateyInstall.ps1
+++ packages/wudt/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name = "wudt"
-$url  = "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wudt&DownloadId=96313&FileTime=129048694816630000&Build=20959"
-$kind = "EXE"
+﻿$id     = "wudt"
+$url    = "https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wudt&DownloadId=96313&FileTime=129048694816630000&Build=20959"
+$kind   = "EXE"
 $silent = "/Q"
 
-Install-ChocolateyPackage $name $kind $silent $url
+Install-ChocolateyPackage -PackageName $id -FileType $kind -SilentArgs $silent -Url $url
